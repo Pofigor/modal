a2b2afe1fcfb4a0989e556d00f3cf7046bf52ed9
Aslanbek <kaipaeff@gmail.com>
2022-09-04 11:21:02 +0300

    finish and deployed

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './index.scss';
 const Modal = ({ open, setOpen, children }) => (
 
   <div className={`overlay animated ${open ? 'show' : ''}`}>
-
+    
     <div className="modal">
-      <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
-        <title />
-        <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+      <svg onClick={() => setOpen(false)} width="50" height="50" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="2.0354" y="2.5" width="31" height="31" rx="7.5" fill="white" stroke="#E0E0E0" />
+        <path d="M26.5713 23.2431L21.0058 17.6776L26.5711 12.1124C28.7171 9.96634 25.3889 6.63811 23.2428 8.78412L17.6776 14.3494L12.1121 8.78393C9.96657 6.63839 6.63846 9.96651 8.78399 12.112L14.3495 17.6775L8.78419 23.2428C6.63818 25.3888 9.96641 28.717 12.1124 26.571L17.6777 21.0057L23.2432 26.5712C25.389 28.717 28.7171 25.3889 26.5713 23.2431Z" fill="#C4C4C4" />
       </svg>
       {children}
     </div>
@@ -23,14 +23,12 @@ function App() {
   return (
     <div className="App">
       <button onClick={() => setOpen(true)} className="open-modal-btn">
-        Открыть модальное окно
+        Click me
       </button>
 
       <Modal open={open} setOpen={setOpen}>
         <img src="./img/travolta.gif" alt="gif" />
-        <h3>Не расстраивай Джона, нажми OK</h3>
-        <button>OK</button>
-        <button>Cancel</button>
+        <img src="./img/romashki.jpg" className='back' alt="Romashki"/>
       </Modal>
 
     </div>
